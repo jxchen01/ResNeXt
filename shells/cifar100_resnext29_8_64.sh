76a13f6a57d9ed905670566653787a3d64ba81ff
Xuanyi Dong <you@example.com>
2017-04-27 16:38:02 +1000

    add some scripts

---
+++ shells/cifar100_resnext29_8_64.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+model=resnext29_8_64
+dataset=cifar100
+epochs=310
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py ./data/cifar.python --dataset ${dataset} --arch ${model} \
+	--save_path ./snapshots/${dataset}_${model}_${epochs} --epochs ${epochs} \
+	--learning_rate 0.1 --schedule 150 225 300 --gammas 0.1 0.1 0.1 --batch_size 128 --workers 2 --ngpu 4
---
+++ shells/resnet20.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+model=resnet20
+dataset=cifar10
+epochs=164
+
+CUDA_VISIBLE_DEVICES=0 python main.py ./data/cifar.python \
+	--dataset ${dataset} --arch ${model} --save_path ./snapshots/${dataset}_${model}_${epochs} --epochs ${epochs} \
+       	--schedule 82 123 --gammas 0.1 0.1 --learning_rate 0.1 --decay 0.0001 --batch_size 128 --workers 2 --ngpu 1
