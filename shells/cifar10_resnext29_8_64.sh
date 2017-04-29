a96b6fc022cd519580e8827fe5104ced67763f79
D-X-Y <280835372@qq.com>
2017-04-29 16:08:05 +1000

    Update README

--- shells/cifar10_resnext29_8_64.sh
+++ shells/cifar10_resnext29_8_64.sh
@@ -5,4 +5,4 @@ epochs=310
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py ./data/cifar.python \
 	--dataset ${dataset} --arch ${model} --save_path ./snapshots/${dataset}_${model}_${epochs} --epochs ${epochs} --learning_rate 0.05 \
-	--schedule 150 225 300 --gammas 0.1 0.1 0.1 --batch_size 128 --workers 2 --ngpu 4
+	--schedule 150 225 300 --gammas 0.1 0.1 0.1 --batch_size 64 --workers 2 --ngpu 4
